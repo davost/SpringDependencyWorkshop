bd142d7dd6e33d2c8f8e3023c59d148d459629c9
davost <bildermejl@gmail.com>
2022-06-08 15:20:35 +0200

    2nd commit part 3 with a few tweaks (but not test and fully implemented methods) with no errrors. :)

--- src/main/java/se/lexicon/config/ComponentScanConfig.java
+++ src/main/java/se/lexicon/config/ComponentScanConfig.java
@@ -3,6 +3,7 @@ package se.lexicon.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import se.lexicon.data_access.StudentDAO;
 
 import java.util.Scanner;
 
@@ -11,9 +12,14 @@ import java.util.Scanner;
 
 public class ComponentScanConfig {
 
-/*@Bean
-  public void Scanner() {
-*/
+  private final StudentDAO studentDAO;
+
+  /*@Bean
+    public void Scanner() {
+  */
+  public ComponentScanConfig(StudentDAO studentDAO) {
+    this.studentDAO = studentDAO;
+  }
 
   @Bean
   public Scanner scanner() {return new Scanner(System.in);}
--- src/main/java/se/lexicon/data_access/StudentDAO.java
+++ src/main/java/se/lexicon/data_access/StudentDAO.java
@@ -1,11 +1,12 @@
 package se.lexicon.data_access;
 
 
+import org.springframework.stereotype.Component;
 import se.lexicon.models.Student;
 
 import java.util.List;
 import java.util.Optional;
-
+@Component
 public interface StudentDAO {
 
     Student save(Student student);
